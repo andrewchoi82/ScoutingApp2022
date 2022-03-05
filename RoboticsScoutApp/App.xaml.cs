efec270a36f181e8b7c1c40fa18d4427fd7130ab
Andrew Choi <97570854+andrewchoi82@users.noreply.github.com>
2022-03-05 14:57:03 -0500

    Add files via upload

---
+++ RoboticsScoutApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using RoboticsScoutApp.Services;
+using RoboticsScoutApp.Views;
+
+namespace RoboticsScoutApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new StartPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
